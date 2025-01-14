a8eeb747b2b8573ffde02efe6641b2664c913b49
SunInTeo <teotu123@gmail.com>
2025-01-14 21:37:46 +0200

    repaired the db path

---
+++ classes/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$username = 'root'; #default
+$password = ''; #default
+$dbname = '../db/database_structure.sql';
+
+try {
+    // Establish database connection using PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
