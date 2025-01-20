2d32cddce20e7580a08bae086c8cecd3dcc29dc1
SunInTeo <teotu123@gmail.com>
2025-01-20 22:36:27 +0200

    restructured the db so it enables topic proposals storage

--- db/database_structure.sql
+++ db/database_structure.sql
@@ -1,4 +1,5 @@
-CREATE TABLE Users (
+CREATE TABLE
+    Users (
         user_id INT AUTO_INCREMENT PRIMARY KEY,
         user_type ENUM ('student', 'teacher') NOT NULL,
         faculty_number VARCHAR(20),
@@ -12,7 +13,8 @@ CREATE TABLE Users (
         project_presentation_datetime DATETIME
     );
 
-CREATE TABLE Essays (
+CREATE TABLE
+    Essays (
         essay_id INT AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
         resources TEXT,
@@ -25,14 +27,15 @@ CREATE TABLE Essays (
         user_id INT NOT NULL
     );
 
-CREATE TABLE FAQ (
+CREATE TABLE
+    FAQ (
         faq_id INT AUTO_INCREMENT PRIMARY KEY,
         question TEXT NOT NULL,
         answer TEXT NOT NULL
     );
 
-
-CREATE TABLE PresentationDays (
+CREATE TABLE
+    PresentationDays (
         day_id INT AUTO_INCREMENT PRIMARY KEY,
         day_date DATE NOT NULL,
         start_time TIME NOT NULL,
@@ -41,40 +44,51 @@ CREATE TABLE PresentationDays (
         presentation_type ENUM ('Essay', 'Project') NOT NULL
     );
 
-CREATE TABLE Projects (
-    project_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    example_distribution_1 TEXT,
-    example_distribution_2 TEXT,
-    example_distribution_3 TEXT,
-    integration TEXT,
-    requirements TEXT,
-    comments TEXT
-);
+CREATE TABLE
+    Projects (
+        project_id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        example_distribution_1 TEXT,
+        example_distribution_2 TEXT,
+        example_distribution_3 TEXT,
+        integration TEXT,
+        requirements TEXT,
+        comments TEXT
+    );
 
-CREATE TABLE Teams (
-    team_id INT AUTO_INCREMENT PRIMARY KEY,
-    project_id INT NOT NULL, 
-    team_comments TEXT, 
-    sample_distribution_1 TEXT, 
-    sample_distribution_2 TEXT,
-    sample_distribution_3 TEXT,
-    FOREIGN KEY (project_id) REFERENCES Projects(project_id) ON DELETE CASCADE
-);
+CREATE TABLE
+    Teams (
+        team_id INT AUTO_INCREMENT PRIMARY KEY,
+        project_id INT NOT NULL,
+        team_comments TEXT,
+        sample_distribution_1 TEXT,
+        sample_distribution_2 TEXT,
+        sample_distribution_3 TEXT,
+        FOREIGN KEY (project_id) REFERENCES Projects (project_id) ON DELETE CASCADE
+    );
 
-CREATE TABLE TeamMembers (
-    team_member_id INT AUTO_INCREMENT PRIMARY KEY,
-    team_id INT NOT NULL, 
-    user_id INT NOT NULL, 
-    FOREIGN KEY (team_id) REFERENCES Teams(team_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
-);
+CREATE TABLE
+    TeamMembers (
+        team_member_id INT AUTO_INCREMENT PRIMARY KEY,
+        team_id INT NOT NULL,
+        user_id INT NOT NULL,
+        FOREIGN KEY (team_id) REFERENCES Teams (team_id) ON DELETE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE
+    );
 
+CREATE TABLE
+    ProposedTopics (
+        topic_id INT AUTO_INCREMENT PRIMARY KEY,
+        proposal_type ENUM ('Essay', 'Project') NOT NULL,
+        topic_label VARCHAR(255) NOT NULL,
+        topic_info TEXT NOT NULL,
+        proposed_by_user_id INT NOT NULL,
+        proposed_by_user_name TEXT NOT NULL,
+        FOREIGN KEY (proposed_by) REFERENCES Users (user_id) ON DELETE CASCADE,
+    );
 
-ALTER TABLE Users
-ADD CONSTRAINT fk_essay FOREIGN KEY (essay_id) REFERENCES Essays (essay_id) ON DELETE SET NULL,
+ALTER TABLE Users ADD CONSTRAINT fk_essay FOREIGN KEY (essay_id) REFERENCES Essays (essay_id) ON DELETE SET NULL,
 ADD CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES Projects (project_id) ON DELETE SET NULL;
 
-ALTER TABLE Essays
-ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE;
+ALTER TABLE Essays ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE;
